87e29b5c7e2bc6aa3743087a918d403c4072d39e
moonxxpower <su551732@gmail.com>
2025-06-10 00:36:06 +0900

    [INIT] 프로젝트 초기 설정

---
+++ src/main/java/com/server/running_handai/RunningHandaiApplication.java
@@ -0,0 +1,13 @@
+package com.server.running_handai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningHandaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningHandaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/running_handai/RunningHandaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.running_handai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningHandaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
