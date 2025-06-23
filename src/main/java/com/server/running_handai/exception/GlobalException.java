c64e161e14c1c7d98d7375101d2fc03233b07261
moonxxpower <su551732@gmail.com>
2025-06-23 21:44:26 +0900

    [SCRUM-142] FEAT: Global Exception 클래스 추가 (#13)
    애플리케이션 전역에서 발생하는 예외를 효과적으로 처리하기 위한 전역 예외 클래스를 추가했습니다.
    추후 자주 발생하는 예외 유형에 대한 핸들러를 추가할 수 있습니다.

--- src/main/java/com/server/running_handai/exception/GlobalException.java
+++ src/main/java/com/server/running_handai/exception/GlobalException.java
@@ -3,6 +3,10 @@ package com.server.running_handai.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +20,63 @@ public class GlobalException {
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         ErrorCode errorCode = e.getErrorCode();
         return getErrorResponse(e, errorCode.getHttpStatus(), errorCode.getCode());
+    }
+
+    /**
+     * BAD_REQUEST (400)
+     * IllegalArgumentException: 사용자가 값을 잘못 입력한 경우
+     * MethodArgumentNotValidException: 전달된 값이 유효하지 않은 경우
+     * HttpMessageNotReadableException: 잘못된 형식으로 요청할 경우
+     * MissingServletRequestParameterException: 필수 요청 매개변수가 누락된 경우
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return getErrorResponse(e, HttpStatus.BAD_REQUEST, "ILLEGAL_ARGUMENT");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        return getErrorResponse(e, HttpStatus.BAD_REQUEST, "METHOD_ARGUMENT_NOT_VALID");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException e) {
+        return getErrorResponse(e, HttpStatus.BAD_REQUEST, "HTTP_MESSAGE_NOT_READABLE");
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException e) {
+        return getErrorResponse(e, HttpStatus.BAD_REQUEST, "MISSING_SERVLET_REQUEST_PARAMETER");
+    }
+
+    /**
+     * METHOD_NOT_ALLOWED (405)
+     * HttpRequestMethodNotSupportedException: 잘못된 Http Method를 가지고 요청할 경우
+     */
+    // spotless:on
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e) {
+        return getErrorResponse(e, HttpStatus.METHOD_NOT_ALLOWED, "HTTP_REQUEST_METHOD_NOT_SUPPORTED");
+    }
+
+    /**
+     * INTERNAL_SERVER_ERROR (500)
+     * RuntimeException
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        return getErrorResponse(e, HttpStatus.INTERNAL_SERVER_ERROR, "REQUEST_SERVER");
+    }
+
+    // 예상하지 못한 모든 예외를 처리
+    // 추후 자주 발생하는 오류에 대해 추가 필요
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return getErrorResponse(e, HttpStatus.INTERNAL_SERVER_ERROR, "REQUEST_SERVER");
     }
 
     // 일관된 예외 응답 처리
