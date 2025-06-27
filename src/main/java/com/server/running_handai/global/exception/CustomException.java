4b008389c71033278d050c23ab5da2e32e83e6bb
ssggii <haan7525@gmail.com>
2025-06-27 18:03:36 +0900

    [SCRUM-181] CHORE: exception 디렉토리 경로 변경(#22)
    exception 디렉토리를 /global의 하위 디렉토리로 변경했습니다.

--- src/main/java/com/server/running_handai/global/exception/CustomException.java
+++ src/main/java/com/server/running_handai/global/exception/CustomException.java
@@ -1,4 +1,4 @@
-package com.server.running_handai.exception;
+package com.server.running_handai.global.exception;
 
 import lombok.Getter;
 
--- src/main/java/com/server/running_handai/global/exception/GlobalException.java
+++ src/main/java/com/server/running_handai/global/exception/GlobalException.java
@@ -1,4 +1,4 @@
-package com.server.running_handai.exception;
+package com.server.running_handai.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
