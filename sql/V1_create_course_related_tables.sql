1bfebd78599669f713bcaacc356e9379a0999425
ssggii <haan7525@gmail.com>
2025-06-27 22:43:59 +0900

    [SCRUM-181] FEAT: Course 관련 테이블 생성 DDL 추가 (#22)
    Course, CourseImage, TrackPoint 엔티티에 대한 데이터베이스 테이블을 생성하는 DDL을 추가했습니다. 이 스크립트는 운영환경 배포 시 수동으로 실행되며, 팀원 간의 스키마 공유 및 버전 관리를 목적으로 합니다.

---
+++ sql/V1_create_course_related_tables.sql
@@ -0,0 +1,47 @@
+-- Version 1: 프로젝트 초기 테이블 생성
+-- 1. course 테이블 생성
+CREATE TABLE course
+(
+    course_id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    created_at     DATETIME(6)                     NOT NULL,
+    updated_at     DATETIME(6)                     NOT NULL,
+    distance       INT                             NOT NULL,
+    duration       INT                             NOT NULL,
+    district       VARCHAR(255)                    NOT NULL,
+    external_id    VARCHAR(255)                    NULL,
+    name           VARCHAR(255)                    NOT NULL,
+    road_condition TEXT                            NOT NULL,
+    tour_point     TEXT                            NULL,
+    traveler_info  TEXT                            NULL,
+    level          ENUM ('EASY', 'HARD', 'MEDIUM') NOT NULL,
+    CONSTRAINT UK_course_external_id UNIQUE (external_id)
+);
+
+
+-- 2. course_image 테이블 생성
+CREATE TABLE course_image
+(
+    course_img_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    course_id     BIGINT       NOT NULL,
+    created_at    DATETIME(6)  NOT NULL,
+    updated_at    DATETIME(6)  NOT NULL,
+    url           VARCHAR(255) NOT NULL,
+    CONSTRAINT UK_course_image_course_id UNIQUE (course_id),
+    CONSTRAINT FK_course_image_to_course FOREIGN KEY (course_id) REFERENCES course (course_id)
+);
+
+
+-- 3. track_point 테이블 생성
+CREATE TABLE track_point
+(
+    track_point_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    course_id      BIGINT      NOT NULL,
+    created_at     DATETIME(6) NOT NULL,
+    updated_at     DATETIME(6) NOT NULL,
+    lat            DOUBLE      NOT NULL,
+    lon            DOUBLE      NOT NULL,
+    ele            DOUBLE      NULL,
+    sequence       INT         NULL,
+    CONSTRAINT FK_track_point_to_course FOREIGN KEY (course_id) REFERENCES course (course_id)
+);
+
