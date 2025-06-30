bf447504f1bff998f5b592417fdf1a8be9e4633b
ssggii <haan7525@gmail.com>
2025-06-30 22:09:48 +0900

    [SCRUM-173] FEAT: 트랙포인트 동기화 API 구현 (#24)
    트랙포인트 동기화 API를 구현했습니다. gpxPath가 null이 아니면서 트랙포인트 리스트가 없는 경우 GPX 파일을 파싱해서 트랙포인트들을 저장합니다.

--- src/main/java/com/server/running_handai/course/controller/CourseDataController.java
+++ src/main/java/com/server/running_handai/course/controller/CourseDataController.java
@@ -1,7 +1,10 @@
 package com.server.running_handai.course.controller;
 
 import com.server.running_handai.course.service.CourseDataService;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +17,19 @@ public class CourseDataController {
 
     private final CourseDataService courseDataService;
 
-    @PostMapping("/synchronize")
-    public ResponseEntity<String> synchronizeCourses() {
+    @PostMapping("/sync-courses")
+    public ResponseEntity<Map<String, String>> synchronizeCourses() {
         courseDataService.synchronizeCourseData();
-        return ResponseEntity.accepted()
-                .body("Course data synchronization has been initiated in the background.");
+        String message = "코스 데이터 동기화 작업이 시작되었습니다. 서버 로그를 통해 진행 상황을 확인하세요.";
+        Map<String, String> responseBody = Map.of("message", message);
+        return ResponseEntity.accepted().body(responseBody);
+    }
+
+    @PostMapping("/sync-trackpoints")
+    public ResponseEntity<Map<String, String>> saveTrackPoints() {
+        courseDataService.syncAllCoursesWithEmptyTrackPoints();
+        String message = "트랙포인트 동기화 작업이 시작되었습니다. 서버 로그를 통해 진행 상황을 확인하세요.";
+        Map<String, String> responseBody = Map.of("message", message);
+        return ResponseEntity.accepted().body(responseBody);
     }
 }
