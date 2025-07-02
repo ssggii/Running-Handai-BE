428666d229947b3e9ca898efd066d5e96ee5c809
moonxxpower <su551732@gmail.com>
2025-07-02 21:37:35 +0900

    [SCRUM-179] FEAT: RoadCondition Entity로 분리 (#21)
    하나의 Course에 대해 5개의 길 상태 설명이 필요하여 RoadCondition을 별도의 Entity로 분리하고 Course와 1:N 관계로 설정하였습니다.

--- sql/V1_create_course_related_tables.sql
+++ sql/V1_create_course_related_tables.sql
@@ -10,7 +10,6 @@ CREATE TABLE course
     district       VARCHAR(255)                    NOT NULL,
     external_id    VARCHAR(255)                    NULL,
     name           VARCHAR(255)                    NOT NULL,
-    road_condition TEXT                            NOT NULL,
     tour_point     TEXT                            NULL,
     level          ENUM ('EASY', 'HARD', 'MEDIUM') NOT NULL,
     CONSTRAINT UK_course_external_id UNIQUE (external_id)
@@ -44,3 +43,13 @@ CREATE TABLE track_point
     CONSTRAINT FK_track_point_to_course FOREIGN KEY (course_id) REFERENCES course (course_id)
 );
 
+-- 4. road_condition 테이블 생성
+CREATE TABLE road_condition
+(
+    road_condition_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    course_id         BIGINT      NOT NULL,
+    created_at        DATETIME(6) NOT NULL,
+    updated_at        DATETIME(6) NOT NULL,
+    description       TEXT        NOT NULL,
+    CONSTRAINT FK_road_condition_to_course FOREIGN KEY (course_id) REFERENCES course (course_id)
+);
