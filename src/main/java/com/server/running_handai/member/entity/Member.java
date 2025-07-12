6563c99b90c39fd560e07b92972db08a5343f1e8
moonxxpower <su551732@gmail.com>
2025-07-12 12:55:04 +0900

    FEAT: member entity 생성 (#44)
    Member 엔티티를 생성하고, provider와 role 필드를 enum 타입으로 추가하였습니다.

---
+++ src/main/java/com/server/running_handai/member/entity/Member.java
@@ -0,0 +1,36 @@
+package com.server.running_handai.member.entity;
+
+import com.server.running_handai.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "provider_id", unique = true, nullable = false)
+    private String providerId; // 플랫폼에서 받아오는 회원 식별자
+
+    @Column(name = "email", unique = true, nullable = false, length = 100)
+    private String email; // 이메일
+
+    @Column(name = "nickname", unique = true, nullable = false, length = 20)
+    private String nickname; // 닉네임
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role; // 회원 역할
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider", nullable = false)
+    private Provider provider; // 인증 제공자
+}
---
+++ src/main/java/com/server/running_handai/member/entity/Provider.java
@@ -0,0 +1,14 @@
+package com.server.running_handai.member.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Provider {
+    GOOGLE("구글"),
+    NAVER("네이버"),
+    KAKAO("카카오");
+
+    private final String description;
+}
