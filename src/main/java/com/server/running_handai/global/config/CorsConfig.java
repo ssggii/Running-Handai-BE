bcf31609a954d317965752170a9de3a05fe8f926
moonxxpower <su551732@gmail.com>
2025-07-19 03:46:18 +0900

    [SCRUM-199] CHORE: profile 설정 파일 중 중복 항목 통합 (#44)
    application-local.yml과 application-prod.yml의 중복 설정을 application.yml로 통합하였습니다.
    또한, CORS 설정 중 allowed-origins를 application.yml에 선언해 환경별 필요한 값만 profile에서 override하도록 구조를 개선하였습니다.

--- src/main/java/com/server/running_handai/global/config/CorsConfig.java
+++ src/main/java/com/server/running_handai/global/config/CorsConfig.java
@@ -1,20 +1,31 @@
 package com.server.running_handai.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${cors.allowed-origins")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        // 쉼표(,) 기준으로 공백 제거 후 나누기
+        List<String> origins = Arrays.stream(allowedOrigins.split(","))
+                .map(String::trim)
+                .toList();
+
+        corsConfiguration.setAllowedOrigins(origins);
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
