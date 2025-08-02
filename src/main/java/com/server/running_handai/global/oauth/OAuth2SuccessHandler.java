f9d9353c1d76aba153c76dbcd858ad51e1498bb1
moonxxpower <su551732@gmail.com>
2025-08-02 14:10:14 +0900

    REFACTOR: 리다이렉트 URI 분기 처리 방식 switch문으로 변경 (#81)

--- src/main/java/com/server/running_handai/global/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/server/running_handai/global/oauth/OAuth2SuccessHandler.java
@@ -3,6 +3,8 @@ package com.server.running_handai.global.oauth;
 import com.server.running_handai.global.jwt.JwtProvider;
 import com.server.running_handai.domain.member.entity.Member;
 import com.server.running_handai.domain.member.repository.MemberRepository;
+import com.server.running_handai.global.response.ResponseCode;
+import com.server.running_handai.global.response.exception.BusinessException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -32,18 +34,20 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication)
             throws IOException {
         String state = httpServletRequest.getParameter("state");
-        String redirectUri = "";
 
         // 쉼표(,) 기준으로 공백 제거 후 나누기
         List<String> redirectUriList = Arrays.stream(redirectUris.split(","))
                 .map(String::trim)
                 .toList();
 
-        if ("local".equalsIgnoreCase(state)) {
-            redirectUri = redirectUriList.getFirst();
-        } else if ("prod".equalsIgnoreCase(state)) {
-            redirectUri = redirectUriList.getLast();
-        }
+        String redirectUri = switch (state.toLowerCase()) {
+            case "prod" -> redirectUriList.getLast();
+            case "local" -> redirectUriList.getFirst();
+            default -> {
+                log.warn("[OAuth2 인증] 유효하지 않은 state 값입니다: {}", state);
+                throw new BusinessException(ResponseCode.BAD_REQUEST_STATE_PARAMETER);
+            }
+        };
         log.info("[OAuth2 인증] 리다이렉트 URI 설정: state={}, URI={}", state, redirectUri);
 
         CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
